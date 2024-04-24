73f297c29aa77517c05706ee154c2dafd7e5ed1e
Bnks10061998 <banakasu4scot@gmail.com>
2024-04-24 10:10:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# Day 5 Task
+
+## **1.Resume Creation**
+[Create your own resume data in JSON format](Resume.js)       
+
+## **2.JSON iterate**
+[For the given JSON iterate over all for loops (for, for in, for of, forEach)](Iterate.js)
---
+++ Resume.js
@@ -0,0 +1,53 @@
+let myResume={
+    "basics": {
+      "name": "KAVIYA B",
+      "email": "banakasu4scot@gmail.com",
+      "phone": 8760561318,
+      "degree": "M.C.A",
+      "location": {
+        "address": "K.K Nagar",
+        "postalCode": 600204,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "www.linkedin.com/in/kaviya-balasubramaniyan-bb4a431a2/",
+          "github":"https://github.com/Bnks10061998"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Winsoft Soltion",
+        "position": "Software Developer",
+        "startDate": "2021-03-17",
+        "endDate": "2024-03-04",
+      },
+    ],
+    "education": [
+      {
+        "institution": "P.S.N.A College Of Engineering and Technology",
+        "department": "Computer Application",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2022,
+        "gpa": 8.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "C,Dot net",
+        "level": "beginer",
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ]
+    
+  }
+  console.log(myResume);
